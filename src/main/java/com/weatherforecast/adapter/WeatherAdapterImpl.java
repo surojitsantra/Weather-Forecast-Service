ce1c298af97c41c3430cc852e8dc0072abfaf95a
Surojit Santra <surojit.santra@smartbear.com>
2024-03-06 12:55:04 +0530

    Added null check for client response.

--- src/main/java/com/weatherforecast/adapter/WeatherAdapterImpl.java
+++ src/main/java/com/weatherforecast/adapter/WeatherAdapterImpl.java
@@ -21,12 +21,18 @@ public class WeatherAdapterImpl implements WeatherAdapter {
         WeatherClientData weatherClientData = this.weatherClient.getWeatherInfo(zipcode);
         log.info("Weather data from client: {}", weatherClientData);
 
+        return weatherInfoMapper(weatherClientData);
+    }
+
+    private WeatherInfo weatherInfoMapper(WeatherClientData weatherClientData) {
         //convert the response from the client to the WeatherInfo object
         WeatherInfo weather = new WeatherInfo();
+        if(weatherClientData == null || weatherClientData.getData() == null || weatherClientData.getData().getValues() == null) {
+            return weather;
+        }
         weather.setCurrentTemperature(weatherClientData.getData().getValues().getTemperature());
         weather.setHighTemperature(weatherClientData.getData().getValues().getTemperatureApparent());
         weather.setLowTemperature(weatherClientData.getData().getValues().getDewPoint());
-
         return weather;
     }
 }
